ac8ca8331b195a95be0c91e92d741455ade1e800
Wednis <150148936+Wednis@users.noreply.github.com>
2024-05-16 17:52:00 +0800

    Update while-for-requests1.py

--- test-code/while-for-requests1.py
+++ test-code/while-for-requests1.py
@@ -5,7 +5,7 @@ import requests
 count=0
 t1=int(time.time())
 while 1:
-    re=requests.get(url="http://61.147.171.105:65468/")
+    re=requests.get(url="http://xxxxxx")
     t2=int(time.time())
     count+=1
     if (t2-t1) == 1:
