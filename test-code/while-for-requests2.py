1dc20e43aaccb92278568d20c93b332c79c213b0
Wednis <150148936+Wednis@users.noreply.github.com>
2024-05-16 17:52:20 +0800

    Update while-for-requests2.py

--- test-code/while-for-requests2.py
+++ test-code/while-for-requests2.py
@@ -1,7 +1,7 @@
 #采用session结合while，达到六秒上下一百个请求
 #在while中嵌入判断导致了速度的下降，果然还是不能用while
 session=requests.Session()
-url="http://61.147.171.105:49872/"
+url="http://xxxxxx"
 count=0
 start = time.time()
 while 1:
