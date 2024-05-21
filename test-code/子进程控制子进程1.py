308f2469f3da8a88475bab21e2c2b0f2c24de666
Wednis <150148936+Wednis@users.noreply.github.com>
2024-05-21 22:34:56 +0800

    Update 子进程控制子进程1.py

--- test-code/子进程控制子进程1.py
+++ test-code/子进程控制子进程1.py
@@ -1,3 +1,4 @@
+#这个控制有点垃圾来着，下次尝试使用local来弄个全局变量
 import threading
 
 def a():
@@ -29,7 +30,7 @@ stop_threads = False
 if __name__ == '__main__':
     for thr in threads:
         thr.start()
-    if (a()) == 2:
+    if (a()) == 2:          #这里写的不好，里面的a()是会执行的
         stop_threads = True
     for thr in threads:
         thr.join()   
